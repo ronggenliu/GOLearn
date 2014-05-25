f2306d352f648f61285c402f42b20e470455de3a
Ronggen Liu <ronggenliu@gmail.com>
2014-05-25 21:37:15 +0800

    update comment.

--- fileop.go
+++ fileop.go
@@ -45,7 +45,7 @@ BEGINFOR:
 	fmt.Printf("%s", bytes)
 }
 
-// try...catch...module
+// try...catch...model
 func Try(fun func(), handler func(interface{})) {
 	defer func() {
 		if err := recover(); err != nil {
